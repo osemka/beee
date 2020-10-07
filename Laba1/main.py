3b7116748fb1920ed3d8dcd7606602ed521e8c96
osemka <70883527+osemka@users.noreply.github.com>
2020-10-07 08:21:47 +0500

    elif

--- Laba1/main.py
+++ Laba1/main.py
@@ -1,2 +1,25 @@
 print('Hello World')
+a = 8
+b = 8
+print (a+b)
+print (a)
+if a > b:
+    print("Чокопай")
+elif a == b:
+    print("Печенье")
+else:
+    print("Конфета")
+d = 18
+c = 7
+if d > c + 10:
+    print("Шоколад")
+name = "Дильназ"
+height = 1.63
+weight = 36
+bmi = weight / (height * height )
+print ("Индекс массы тела: " + str(bmi))
+if bmi < 25:
+    print("y " + name + " нет лишнего веса")
+else:
+    print("y " + name + "есть лишний вес") 
 
